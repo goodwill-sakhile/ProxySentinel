dc32434d160e0189001d4cb2db9371c8ee2f58fc
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-04-19 00:56:26 +0200

    main script

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ root = Builder.load_string("""
                     theme_text_color:"Custom"
                     text_color:1, 1, 1, 1
                     md_bg_color:0, 220/float(255), 154/float(255), 1
+                    
                 MDIconButton:
                     icon:"av-timer"
                     pos_hint:{"center_x":.5, "center_y":.5}
@@ -59,52 +60,55 @@ root = Builder.load_string("""
                     text_color:1, 1, 1, 1
                 Widget:
         MDBoxLayout:
-            radius:10, 10, 0, 0
-            Widget:
-            MDBoxLayout:
-                size_hint_x:None
-                width:"400dp"
-                orientation:"vertical"
-                spacing: 10
-                MDBoxLayout:
-                    size_hint_y:None
-                    height:"100dp"
-                MDBoxLayout:
-                    size_hint_y:None
-                    height:"50dp"
-                    MDLabel:
-                        text:"Enter Search Words"
-                        font_name:"BeautifulPeoplePersonalUse-dE0g"
-                        text_size:self.size
-                        halign:"center"
-                        valign:"middle"
-                        color:1, 1, 1, 1
-                MDTextField:
-                    id: name_field
-                    helper_text: "This will be used to personalize your profile"
-                    helper_text_mode: "on_focus"
-                    icon_right: "account"
-                    size_hint_x: 1
-                    mode: "rectangle"
-                    fill_color: 0.1, 0.1, 0.1, 0.05
-                    text_color: 1, 1, 1, 1
-                    hint_text_color_focus: 1, 0.5, 0, 1
-                    #line_color_focus: 0, 154/float(255), 220/float(255), 1
-                    line_color_normal:0, 154/float(255), 220/float(255), 1
-                    icon_right:"magnify"
-                MDRaisedButton:
-                    text: "Search Sites to Block"
-                    font_name:"BeautifulPeoplePersonalUse-dE0g"
-                    size_hint_x: 1
-                    size_hint_y: None
-                    height: "50dp"
-                    md_bg_color: 0, 220/float(255), 154/float(255), 1  # Orange background
-                    text_color: 1, 1, 1, 1     # White text
-                    font_size: "18sp"
-                    radius: [12, 12, 12, 12]  # Rounded corners
-                    elevation: 4
-                Widget:
-            Widget:
+            ScreenManager
+                MDScreen:
+                    name:"block_site"
+                    MDBoxLayout:
+                        Widget:
+                        MDBoxLayout:
+                            size_hint_x:None
+                            width:"400dp"
+                            orientation:"vertical"
+                            spacing: 10
+                            MDBoxLayout:
+                                size_hint_y:None
+                                height:"100dp"
+                            MDBoxLayout:
+                                size_hint_y:None
+                                height:"50dp"
+                                MDLabel:
+                                    text:"Enter Search Words"
+                                    font_name:"BeautifulPeoplePersonalUse-dE0g"
+                                    text_size:self.size
+                                    halign:"center"
+                                    valign:"middle"
+                                    color:1, 1, 1, 1
+                            MDTextField:
+                                id: name_field
+                                helper_text: "This will be used to personalize your profile"
+                                helper_text_mode: "on_focus"
+                                icon_right: "account"
+                                size_hint_x: 1
+                                mode: "rectangle"
+                                fill_color: 0.1, 0.1, 0.1, 0.05
+                                text_color: 1, 1, 1, 1
+                                hint_text_color_focus: 1, 0.5, 0, 1
+                                #line_color_focus: 0, 154/float(255), 220/float(255), 1
+                                line_color_normal:0, 154/float(255), 220/float(255), 1
+                                icon_right:"magnify"
+                            MDRaisedButton:
+                                text: "Search Sites to Block"
+                                font_name:"BeautifulPeoplePersonalUse-dE0g"
+                                size_hint_x: 1
+                                size_hint_y: None
+                                height: "50dp"
+                                md_bg_color: 0, 220/float(255), 154/float(255), 1  # Orange background
+                                text_color: 1, 1, 1, 1     # White text
+                                font_size: "18sp"
+                                radius: [12, 12, 12, 12]  # Rounded corners
+                                elevation: 4
+                            Widget:
+                        Widget:
 """)
 class MainBox(MDBoxLayout):
     pass
