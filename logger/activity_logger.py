4486f420a486a3e2cb2d7ddb5b2c6ba7c1158ae9
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-07-06 23:59:46 +0200

    Create activity_logger.py

---
+++ logger/activity_logger.py
@@ -0,0 +1,12 @@
+import datetime
+
+class ActivityLogger:
+    """
+    Logs blocked and allowed requests.
+    """
+    def __init__(self, logfile="logs/blocked_requests.log"):
+        self.logfile = logfile
+
+    def log(self, ip, url, action="BLOCKED"):
+        with open(self.logfile, "a") as f:
+            f.write(f"{datetime.datetime.now()}: {action} {ip} -> {url}\n")
